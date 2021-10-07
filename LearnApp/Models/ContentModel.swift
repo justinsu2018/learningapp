4fefa1d2fbbd6dc42ad0a513472dd2df8000a9ee
sysu <sysu@gmail.com>
2021-10-08 08:53:42 +1100

    a. organise files into folders

---
+++ LearnApp/Models/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  LearnApp
+//
+//  Created by Justin Su on 8/10/21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
--- LearnApp/Views/HomeView.swift
+++ LearnApp/Views/HomeView.swift
@@ -7,15 +7,21 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    
+    @EnvironmentObject var mode:ContentModel
+    
     var body: some View {
+        
+        
         Text("Hello, world!")
             .padding()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
