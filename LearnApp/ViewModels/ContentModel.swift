c8f55b25e7e7ee532bcbb3c6c02fd7afe21393af
sysu <sysu@gmail.com>
2021-10-08 09:35:20 +1100

    add code for read data from local

---
+++ LearnApp/ViewModels/ContentModel.swift
@@ -0,0 +1,50 @@
+//
+//  ContentModel.swift
+//  LearnApp
+//
+//  Created by Justin Su on 8/10/21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecorder = JSONDecoder()
+            let modules = try jsonDecorder.decode([Module].self, from: jsonData)
+            
+            self.modules = modules
+            
+        }
+        catch {
+            print("Couldn't parse local data:\(error)")
+        }
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            let styledata = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styledata;
+        }
+        catch {
+            print ("Couldn't parse style data")
+        }
+    }
+    
+}
