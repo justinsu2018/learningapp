90688ed652487e0b122314589612b511ff49cbf5
sysu <sysu@gmail.com>
2021-10-08 08:37:51 +1100

    Initial Commit

---
+++ LearnApp/LearnAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnAppApp.swift
+//  LearnApp
+//
+//  Created by Justin Su on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
